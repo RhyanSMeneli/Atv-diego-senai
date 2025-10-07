b59e091ca74399fb0c0e19b049e68acc95c298e9
RhyanSMeneli <rhyan.contato2010@gmail.com>
2025-10-07 15:14:07 -0300

    Add files via upload

---
+++ atv-16/atv-16/Program.cs
@@ -0,0 +1,30 @@
+﻿int n1 = 0, n2 = 0, n3 = 0, n4 = 0, i = 0;
+
+Console.WriteLine("Digite um número:");
+n1 = int.Parse(Console.ReadLine()!);
+i = n1 - 1;
+n3 = n1;
+if (n1 < 0)
+{
+    Console.WriteLine("Inexistente!");
+}
+if (n1 == 0)
+{
+    Console.WriteLine("Por definição é 1");
+}
+else if (n1 > 0)
+{
+    while (i > 0)
+    {
+
+        n3 = n3 * i;
+        i--;
+
+
+    }
+
+
+
+    Console.WriteLine($"O fatorial de {n1} é {n3}");
+}
+
